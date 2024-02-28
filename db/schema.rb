b62a8e97a9baf4635008cbbc9891da8772c7a161
John Anders <johnpatrickanders@gmail.com>
2024-02-28 11:52:47 -0500

    add default status to migrations

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_164460) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "public"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -25,7 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_164460) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "public"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
