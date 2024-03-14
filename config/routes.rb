016c9d1788af34889a6e234e4fa557132ffa0117
John Anders <johnpatrickanders@gmail.com>
2024-03-14 18:11:08 -0400

    checkpoint and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  resources :articles do
-    resources :comments
-  end
+  # root "articles#index"
+  # resources :articles do
+  #   resources :comments
+  # end
+
+  mount_ember_app :frontend, to: "/"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
